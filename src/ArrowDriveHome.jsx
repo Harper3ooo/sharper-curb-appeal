6b66a021483b6af8ca9b18d0b1546c428b6773d7
Harper3ooo <iamjayharperco@gmail.com>
2025-04-29 09:26:40 -0500

    over it

--- src/ArrowDriveHome.jsx
+++ src/ArrowDriveHome.jsx
@@ -4,34 +4,35 @@ import './ArrowDriveHome.css';
 export default function ArrowDriveHome() {
   const businesses = [
     {
-      name: 'Bounce Houses & More',
-      path: '/inflatable-business',
-      cta: 'See Inflatables'
+      name: 'Sharper Curb Appeal',
+      path: '/sharper-curb-appeal',
+      image: '/images/curb-thumbnail.png',
+      cta: 'Enter Site'
     },
     {
       name: 'Arrow Drive Shop',
       path: '/arrow-drive',
+      image: '/images/arrowdrive-thumbnail.png',
       cta: 'Shop Now'
     },
     {
-      name: 'Sharper Curb Appeal',
-      path: '/sharper-curb-appeal',
-      cta: 'Enter Site'
+      name: 'Bounce Houses & More',
+      path: '/inflatable-business',
+      image: '/images/inflatable-thumbnail.png',
+      cta: 'See Inflatables'
     }
   ];
 
   return (
     <div className="arrowdrive-home">
-      {/* Logo at Top */}
       <header className="header">
         <img src="/images/arrow-drive-logo.png" alt="Arrow Drive Logo" className="logo" />
       </header>
 
-      {/* Thumbnails (now just solid black sections) */}
-      <main className="thumbnail-container">
+      <main className="thumbnail-grid">
         {businesses.map((biz, idx) => (
           <Link to={biz.path} key={idx} className="thumbnail-link">
-            <div className="thumbnail">
+            <div className="thumbnail" style={{ backgroundImage: `url(${biz.image})` }}>
               <div className="overlay">
                 <h2>{biz.name}</h2>
                 <button>{biz.cta}</button>
@@ -41,16 +42,8 @@ export default function ArrowDriveHome() {
         ))}
       </main>
 
-      </main>
-
-      {/* Footer */}
       <footer className="footer">
-        <div className="footer-contact">
-          arrowdrive@gmail.com | (601) 674-0759
-        </div>
-        <div className="footer-copy">
-          © {new Date().getFullYear()} Arrow Drive. All rights reserved.
-        </div>
+        © {new Date().getFullYear()} Arrow Drive. All rights reserved.
       </footer>
     </div>
   );
