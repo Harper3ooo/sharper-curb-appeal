da170aa7f39063a6c3be9a6b244164c572cfaecd
Harper3ooo <iamjayharperco@gmail.com>
2025-04-28 19:12:44 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import SharperCurbAppeal from './SharperCurbAppeal';
 import FAQPage from './FAQPage';
-// You'll add AthletePage and InflatablesPage later
 
 export default function App() {
   return (
@@ -24,12 +23,8 @@ export default function App() {
       </nav>
 
       <Routes>
-        <Route path="/" element={<ArrowDriveHome />} />
-        <Route path="/curb" element={<SharperCurbAppeal />} />
+        <Route path="/" element={<SharperCurbAppeal />} />
         <Route path="/faq" element={<FAQPage />} />
-        {/* Future expansion */}
-        {/* <Route path="/athlete" element={<AthletePage />} /> */}
-        {/* <Route path="/inflatables" element={<InflatablesPage />} /> */}
       </Routes>
     </Router>
   );
