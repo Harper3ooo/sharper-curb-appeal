f0c46d0a26723c186c1a6f40ed0a61b781c15db5
Harper3ooo <iamjayharperco@gmail.com>
2025-04-30 00:04:00 -0500

    Update ArrowDriveHome.jsx

--- src/ArrowDriveHome.jsx
+++ src/ArrowDriveHome.jsx
@@ -1,23 +1,19 @@
-import { Link } from 'react-router-dom';
 import './ArrowDriveHome.css';
 
 export default function ArrowDriveHome() {
   const businesses = [
     {
       name: 'Sharper Curb Appeal',
-      path: '/sharper-curb-appeal',
       image: '/images/curb-thumbnail.png',
       cta: 'Enter Site'
     },
     {
       name: 'Arrow Drive Shop',
-      path: '/arrow-drive',
       image: '/images/arrowdrive-thumbnail.png',
       cta: 'Shop Now'
     },
     {
       name: 'Bounce Houses & More',
-      path: '/inflatable-business',
       image: '/images/inflatable-thumbnail.png',
       cta: 'See Inflatables'
     }
@@ -31,14 +27,14 @@ export default function ArrowDriveHome() {
 
       <main className="thumbnail-grid">
         {businesses.map((biz, idx) => (
-          <Link to={biz.path} key={idx} className="thumbnail-link">
+          <div key={idx} className="thumbnail-link">
             <div className="thumbnail" style={{ backgroundImage: `url(${biz.image})` }}>
               <div className="overlay">
                 <h2>{biz.name}</h2>
                 <button>{biz.cta}</button>
               </div>
             </div>
-          </Link>
+          </div>
         ))}
       </main>
 
