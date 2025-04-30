ea92900bd8bfb27e99fb10407c23d90d7ccfbe22
Harper3ooo <iamjayharperco@gmail.com>
2025-04-30 09:56:57 -0500

    Damn

--- src/ArrowDriveHome.jsx
+++ src/ArrowDriveHome.jsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import './ArrowDriveHome.css';
 
 export default function ArrowDriveHome() {
   const businesses = [
     {
       name: 'Sharper Curb Appeal',
-      path: '/sharper-curb-appeal',
       image: '/images/curb-thumbnail.png',
       cta: 'Enter Site'
     },
     {
       name: 'Arrow Drive Shop',
-      path: '/arrow-drive',
       image: '/images/arrowdrive-thumbnail.png',
       cta: 'Shop Now'
     },
     {
       name: 'Bounce Houses & More',
-      path: '/inflatable-business',
       image: '/images/inflatable-thumbnail.png',
       cta: 'See Inflatables'
     }
@@ -27,26 +22,19 @@ export default function ArrowDriveHome() {
   return (
     <div className="arrowdrive-home">
       <header className="header">
-        <img
-          src="/images/arrow-drive-logo.png"
-          alt="Arrow Drive Logo"
-          className="logo"
-        />
+        <img src="/images/arrow-drive-logo.png" alt="Arrow Drive Logo" className="logo" />
       </header>
 
       <main className="thumbnail-grid">
         {businesses.map((biz, idx) => (
-          <Link to={biz.path} key={idx} className="thumbnail-link">
-            <div
-              className="thumbnail"
-              style={{ backgroundImage: `url(${biz.image})` }}
-            >
+          <div key={idx} className="thumbnail-link">
+            <div className="thumbnail" style={{ backgroundImage: `url(${biz.image})` }}>
               <div className="overlay">
                 <h2>{biz.name}</h2>
-                <span className="cta">{biz.cta}</span>
+                <button>{biz.cta}</button>
               </div>
             </div>
-          </Link>
+          </div>
         ))}
       </main>
 
