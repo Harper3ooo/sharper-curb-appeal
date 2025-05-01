de9e572489deaf294fe4dbddad6fd6fa9b8fb80e
Harper3ooo <iamjayharperco@gmail.com>
2025-05-01 01:54:22 -0500

    hmmmm

--- api/printful-products.js
+++ api/printful-products.js
@@ -1,6 +1,6 @@
 // api/printful-products.js
 export default async function handler(req, res) {
-    const PRINTFUL_API_KEY = 'your-api-key-here';
+    const PRINTFUL_API_KEY = 'D60crme6Wf5aaWoscHPAnFkL7ZdUakhNuYcZCh4mRrp7Jl7kGg0o5faABW17mYuM';
   
     try {
       const response = await fetch('https://api.printful.com/store/products', {
